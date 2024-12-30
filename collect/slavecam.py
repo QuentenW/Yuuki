768fc736118b62f728fc56af9d0dfb064536b35e
QuentenW <quenten.welch1@ucalgary.ca>
2024-12-30 14:18:55 -0600

    minor fixes

--- collect/slavecam.py
+++ collect/slavecam.py
@@ -6,23 +6,33 @@ import cv2, time, os, signal, pickle, time, pickle
 # gpio pins
 img_pin = 16
 end_pin = 26
+
 # TODO: these args should be from a config, s.t. can be shared
 save_dir = '../data/pusht'
 img_size = (1024, 1024) # (w, h)
 save_img_size = (512, 512)
 camera_fps = 10
 
+# Create a new folder with an iterative name
+existing_folders = [f for f in os.listdir(save_dir) if os.path.isdir(os.path.join(save_dir, f))]
+new_folder_name = f"save_folder_{len(existing_folders) + 1}"
+new_folder_path = os.path.join(save_dir, new_folder_name)
+os.makedirs(new_folder_path)
+
+# Update file paths to save in the new folder
+video_path = os.path.join(new_folder_path, f"video2_{save_id}.mp4")
+pickle_path = os.path.join(new_folder_path, f"times2_{save_id}.pkl")
+
 save_id = time.time()
 camera = init_camera(*img_size)
-video_writer = cv2.VideoWriter(f'{save_dir}/video2_{save_id}.mp4',
+video_writer = cv2.VideoWriter(video_path,
                                 cv2.VideoWriter_fourcc(*'mp4v'),
                                 camera_fps, save_img_size)
 timestamp_data = []
 gpio.setmode(gpio.BCM)
 gpio.setup(img_pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 gpio.setup(end_pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
-# gpio.add_event_detect(img_pin, gpio.RISING, lambda _: snap(camera))
-# gpio.add_event_detect(end_pin, gpio.RISING, lambda _: 1/0) # debounce
+
 while True:
   if gpio.input(img_pin)==gpio.HIGH:
     img = get_image(camera)
@@ -37,8 +47,7 @@ while True:
     video_writer.release()
     camera.stop()
     gpio.cleanup()
-    with open(f'{save_dir}/times2_{save_id}.pkl', 'wb') as file:
+    with open(pickle_path, 'wb') as file:
       pickle.dump(timestamp_data, file)
     break
   time.sleep(0.01)
-
